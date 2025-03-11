c818abebc50f2209c10652cc353606a7782be214
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2025-03-12 01:13:08 +0600

    Dynamically added the 2nd navbar's elements

--- script/index.js
+++ script/index.js
@@ -5,8 +5,21 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
+// {
+//     "category_id":"1001",
+//     "category":"Music"
+// }
+
 function displayCategories(categories) {
-  console.log(categories);
+  const categoryContainer = document.getElementById("category-container");
+  for (const cate of categories) {
+    console.log(cate);
+    categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cate.category}</button>
+    `;
+    categoryContainer.append(categoryDiv);
+  }
 }
 
 loadCategories();
